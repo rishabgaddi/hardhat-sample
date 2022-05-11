aab19c813a8f199b47cb5f06e40dc2a2d6c87b89
rishabgaddi <rishab03@live.com>
2022-05-11 13:46:44 +0200

    Added the functionality to create random zombies and provided an event listner for the front end.

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract ZombieFactory {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10**dnaDigits;
 
@@ -19,11 +21,21 @@ contract ZombieFactory {
      */
     function _createZombie(string memory _name, uint256 _dna) private {
         zombies.push(Zombie(_name, _dna));
+        uint256 id = zombies.length - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str)
         private
         view
         returns (uint256)
-    {}
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 }
